14bc62cd8d8c6e0acdfd10f49dbd61eaed738857
VesnickyTrombonista <11652247@cuni.cz>
2021-12-31 21:42:27 +0100

    Pohyb postavy, neúplná gravitace

--- Game_BubbleZS/Game_BubbleZS.py
+++ Game_BubbleZS/Game_BubbleZS.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import pygame
+import time
+
 # Převod relativní­ cesty vzhledem k tomuto souboru na absolutní­ cestu.
 def fix_path(p):
     import os
@@ -31,45 +33,25 @@ orange = pygame.Color(255, 165, 0)
 darkblue = pygame.Color(0, 0, 128)
 purple = pygame.Color(128, 0, 128)
 
+#pozadí
+background = pygame.image.load("background.jpg").convert() 
 
-# Načteme obrázek míčku, opět dostaneme objekt typu Surface
-# ! pozor: musíme program spouštět ze stejné složky, ve které máme obrázek, jinak se nenačte (bez fix_path) ! 
-# - convert() mění­ vnitřní­ reprezentaci uložení­ obrázku, aby se to nemuselo dělat až při vykreslení -> rychlejší hra.
-#ball_image1 = pygame.image.load(fix_path("ball.png")).convert()
-# ball = pygame.image.load("ball.png")
-# ball_image2 = pygame.image.load(fix_path("ball.png")).convert()
-
-# Vyrobí­me si obdélní­k kolem míčku -- později ho použijeme pro animaci a detekci kolizí.
-# -> dostaneme objekt typu Rect (0, 0, w, h), kde w, h jsou rozměry obrázku (jako int)
-# ball_rect1 = ball_image1.get_rect()
-# print("Ball rect:", ball_rect1) 
-# ball_rect2 = ball_image2.get_rect()
-# print("Ball rect:", ball_rect2)
-# ball_rect3 = ball_image1.get_rect()
-# print("Ball rect:", ball_rect3)
-
-background = pygame.image.load("background.jpg").convert() #...vybrat správný obrázek
-
-
+#ikona hráče
 player = pygame.image.load("shooter.png").convert()
 player.set_colorkey(white)
 
+#kam ho nakreslím
+place = int((width/2)-60)
+motion = 0
+isLeftDown = False
+isRightDown = False
 
-#velocity = pygame.Vector2(8, 11)
-
-#image.set_colorkey((255, 0, 0)) klíč, který nevykreslí barvu pozadí
-# New width and height will be (50, 30).
-#IMAGE_SMALL = pg.transform.scale(IMAGE, (50, 30))
-# Rotate by 0 degrees, multiply size by 2.
-#IMAGE_BIG = pg.transform.rotozoom(IMAGE, 0, 2)
-
-
-
+gravitation = 0.12 #dodělat gravitaci
 # Vytvoří­me hodiny, které hlídají­ kolik času uplynulo od posledního snímku
 clock = pygame.time.Clock()
 
 
-# --- Příprava proměnných pro animaci ---
+# --- Příprava proměnných a tříd pro animaci ---
 class Ball:
     def __init__(self, velocity, center, radius, color):
         self.velocity = velocity
@@ -80,65 +62,116 @@ class Ball:
 
     def draw(self): #nakresli
         pygame.draw.circle(screen, self.color, self.center, self.radius)
+        #if self.velocity.y>0: #dolu
+         #   self.velocity.y += gravitation
+        #if self.velocity.y<0: #nahoru
+        self.velocity.y += gravitation
     
     def bounce(self): #odraz
-        top = max(height-12*self.radius, 0)
+        global gravitation
+        top = max(height-18*self.radius, 0)
         if self.center[0] < self.radius or self.center[0] > width-self.radius:
             self.velocity.x = -self.velocity.x
         if self.center[1] < top+self.radius or self.center[1] > height-self.radius:
             self.velocity.y = -self.velocity.y
-
     def shift(self):  #posuň
         self.center += self.velocity
 
-class Player: #108x165
-    def __init__(self,x,y):
-        self.x=x
-        self.y=y
+class Player: #60x100
+    global place, motion
+    def __init__(self,where):
+        self.where = where
     def draw(self):
-        screen.blit(player,(self.x, self.y))
+        left=-2
+        right=int(width-60)
+        if self.where < left+5:
+           self.where = left
+        if self.where > right-5:
+            self.where = right
+        screen.blit(player,(self.where, height-100))
     def get_topcenter(self):
-        return (width-self.x-50,height-165)
+        return (width-self.where-25, height-100)
     def get_uppercenter(self):
-        return (width-self.x-150,height-165)
-    #if ball_rect.left < 0 or ball_rect.right > width:
-     #   velocity.x = -velocity.x
-    #if ball_rect.top < 0 or ball_rect.bottom > height:
-     #   velocity.y = -velocity.y
+        return (width-self.where-25, height-200)
 
 class Slug: #střela
     def __init__(self,color=red):
         self.color=color
     def draw(self):
-        pygame.draw.line(screen, self.color, Player.get_uppercenter(), PLayer.get_topcenter(),width=2)
+        pygame.draw.line(screen, self.color, gamer.get_uppercenter(), gamer.get_topcenter(), width=2)#...
 
+#zpráva
+font = pygame.font.SysFont(None, 30)
+
+def message(text, color):
+    info = font.render(text, True, color)
+    screen.blit(info, (width/2-180, height/2))
     
 balls=[]
 
 # Vektor rychlosti kuliček (x, y), v "pixelech za snímek", #středy kuliček, #poloměry, #barva
-prvni = Ball(pygame.Vector2(6,6), (420,680), 35, orange)
-druhy = Ball(pygame.Vector2(7,7), (820,580), 70, purple)
-treti = Ball(pygame.Vector2(8,8), (100,640), 25, darkblue)
+prvni = Ball(pygame.Vector2(5,5), (420,680), 35, orange)
+druhy = Ball(pygame.Vector2(6,6), (820,580), 60, purple)
+treti = Ball(pygame.Vector2(7,7), (100,640), 25, darkblue)
 
 
 # --- Nekonečná smyčka animace ---
+message("You play the game Bubble Trouble.", green)
+pygame.display.update()
+time.sleep(2)
+
 while True:
+
+    gamer = Player(place+motion)
+
     # --- UPDATE ---
 
     # Přečteme události, které nastaly od poslední­ho update
     for event in pygame.event.get():
         # A zajistí­me exit při požadavku na zavření okna (křížek)
             if event.type == pygame.QUIT:
+                screen.fill(black)
+                message("You left the game Bubble Trouble.", red)
+                pygame.display.update()
+                time.sleep(2)
                 pygame.quit()
                 print("You left the game.")
                 os._exit(0) 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    screen.fill(black)
+                    message("You left the game Bubble Trouble.", red)
+                    pygame.display.update()
+                    time.sleep(1)
+                    pygame.quit()
+                    print("You left the game.")
+                    os._exit(0) 
 
-            if event.type == pygame.KEYDOWN: # or event.type == pygame.KEYUP:
-                if event.key == pygame.K_SPACE:
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    isLeftDown = False
+                if event.key == pygame.K_RIGHT:
+                    isRightDown = False
+
+
+            pressed = pygame.key.get_pressed()
+
+            if pressed: 
+                if pressed[pygame.K_SPACE]:
                     novy = Ball(pygame.Vector2(7,5), (30,680), 30, lime)
-                if event.key == pygame.K_SPACE:
                     slug=Slug()
-    #space_pressed = pygame.key.get_pressed()[pygame.K_SPACE]
+                    slug.draw()
+                if(pressed[pygame.K_LEFT] or isLeftDown==True):
+                    motion -= 8
+                    isLeftDown = True
+                if(pressed[pygame.K_RIGHT] or isRightDown==True):
+                    motion += 8
+                    isRightDown = True
+
+    if isLeftDown==True:
+        motion -= 8
+    if isRightDown==True:
+        motion += 8
 
 
 
@@ -148,19 +181,19 @@ while True:
     # Vyčistíme obrazovku (nastavíme všude černou)
     screen.fill(black)
 
-
-    #player = Player(200,200)
     screen.blit(background, (0,0))
-    screen.blit(player,((width/2)-80, height-160))
 
-    #ve for cyklu všechno kreslení
+    gamer.draw()
+
+    #ve for cyklu všechno kreslení míčků
+    gravitation*=1.000002
 
     for ball in balls:
         ball.bounce()
         ball.shift()
         ball.draw()
 
-    # Zapíšeme objekt `screen`, "někam", aby se nám zobrazil v okně›
+    # Zapíšeme objekt 'screen', "někam", aby se nám zobrazil v okně›
     # (do teď jsme kreslili jen někam do paměti, teď to zobrazíme na obrazovku)
     pygame.display.flip()
 
